0e496300ea349f3be1d47486ce25eb7005ca49bc
Calvin Wang <calvinwang@yahoo.com>
2021-06-28 12:20:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,22 +7,48 @@ function plusSlides(n) {
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("pictures");
-  // let dots = document.getElementsByClassName("dot");
+  let slides = document.querySelectorAll('.pictures');
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  // for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  // }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+const tabcontent = document.querySelectorAll('.tabcontent');
+const tablinks = document.querySelectorAll('.tablinks');
+let i;
+
+function openCity(evt, cityName) {
+  for(i = 0; i<tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+
+  for(i = 0; i<tablinks.length; i++) {
+    tablinks[i].className.replace(' active','');
+  }
+
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+const responsenav = document.querySelector('.responsenav');
+const sidebar = document.querySelector('.sidebar');
+const bg = document.querySelector('.background');
+
+responsenav.addEventListener('click', function(){
+  sidebar.classList.toggle('sidebar-active');
+  responsenav.classList.toggle('responsenav-active');
+  bg.classList.toggle('background-active');
+});
